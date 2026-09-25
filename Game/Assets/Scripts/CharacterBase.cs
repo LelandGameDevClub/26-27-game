fcc37486d36422729c7a8d3f884c1efd90e8b355
Lam Anh Chu <chuanhlam11@gmail.com>
2026-09-24 21:41:27 -0700

    Created base character class

---
+++ Game/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterBase : MonoBehaviour
+{
+    private Dictionary<string, int> stats =
+    {
+        {"Athletics", 0},
+        {"Will", 0},
+        {"Intellect", 0},
+        {"Charisma", 0}
+    };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
